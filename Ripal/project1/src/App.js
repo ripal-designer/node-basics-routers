7be41670bdbc89f4d2cfb670916cb90ac86f9a05
Akash Patel <apatel@tvo.org>
2022-06-01 14:23:38 -0400

    ds
    dsd

---
+++ Ripal/project1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Test from "./components/Test";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Test></Test>
+      </div>
+    );
+  }
+}
+export default App;
